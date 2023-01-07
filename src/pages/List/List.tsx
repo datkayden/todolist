51b8ccbf0de625c2e755ecd14f10e1cb5f7d8465
datkayden <thanhdatcntt301@gmail.com>
2023-01-07 13:27:06 +0700

    Test useState hook

--- src/pages/List/List.tsx
+++ src/pages/List/List.tsx
@@ -1,8 +1,23 @@
-import React, {ReactElement} from "react";
+import React, {ReactElement, useState} from "react";
 
 const List = (): ReactElement => {
+    const [count, setCount] = useState(0);
+    const addCount = () => {
+        const newCount = count + 1;
+        setCount(newCount);
+    };
+
+    const subCount = () => {
+        const newCount = count - 1;
+        setCount(newCount);
+    };
+
     return (
-        <div>List Page</div>
+        <div>
+            <p>Count: {count}</p>
+            <button onClick={addCount}>Add</button>
+            <button onClick={subCount}>Sub</button>
+        </div>
     );
 };
 
