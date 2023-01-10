6b1ed950256f9a7076a6c4f0d385c4f888a6ea44
datkayden <thanhdatcntt301@gmail.com>
2023-01-10 12:45:39 +0700

    Apply typescript for store

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,6 +1,7 @@
 import {configureStore} from "@reduxjs/toolkit";
 import {combineReducers} from "redux";
 import user from "store/userReducer";
+import {TypedUseSelectorHook, useDispatch, useSelector} from "react-redux";
 
 const reducer = combineReducers({
     user
@@ -9,5 +10,12 @@ const reducer = combineReducers({
 const store = configureStore({
     reducer,
 });
+
+// export type
+export type RootState = ReturnType<typeof reducer>;
+export type AppDispatch = typeof store.dispatch;
+
+export const useAppDispatch: () => AppDispatch = useDispatch;
+export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;
 
 export default store;
