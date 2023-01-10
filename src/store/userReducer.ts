b7fd10902f5591238b83c97fe8fa79501f3e68cb
datkayden <thanhdatcntt301@gmail.com>
2023-01-10 12:45:30 +0700

    Apply typescript for reducer

--- src/store/userReducer.ts
+++ src/store/userReducer.ts
@@ -1,44 +1,55 @@
-import {createSlice} from "@reduxjs/toolkit";
+import {createAction, createAsyncThunk, createSlice, PayloadAction} from "@reduxjs/toolkit";
+import {RootState} from "./index";
+
+interface UserState {
+    count: number;
+    isLoading: boolean;
+}
+
+const initialState: UserState = {
+    count: 0,
+    isLoading: false
+};
+
+export const getAsyncCount = createAsyncThunk("user/fetchById", async (userId: number): Promise<number> => {
+    console.log("userId", userId);
+    await new Promise(resolve => setTimeout(resolve, 3000));
+    return 10;
+});
 
 const slice = createSlice({
     name: "user",
-    initialState: {
-        user: {
-            name: "dat",
-            age: 27
+    initialState,
+    reducers: {
+        increase: state => {
+            state.count += 1;
+        },
+        decrease: state => {
+            state.count -= 1;
+        },
+        increaseByAmount: (state, action: PayloadAction<number>) => {
+            state.count += action.payload;
+        },
+        decreaseByAmountReducer: (state, action: PayloadAction<number>) => {
+            state.count -= action.payload;
         }
     },
-    reducers: {
-        loginSuccess: (state, action) => {
-            state.user = action.payload;
-        },
-        logoutSuccess: (state) => {
-            state.user = {
-                name: "",
-                age: 0
-            };
-        }
+    extraReducers: (builder) => {
+        builder.addCase(getAsyncCount.pending, (state) => {
+            state.isLoading = true;
+        });
+        builder.addCase(getAsyncCount.fulfilled, (state, action: PayloadAction<number>) => {
+            state.isLoading = false;
+            state.count += action.payload;
+        });
     }
 });
 
 export default slice.reducer;
 
-export const {logoutSuccess, loginSuccess} = slice.actions;
+export const {increase, decrease, increaseByAmount} = slice.actions;
 
-const login = (name: string, age: number) => (dispatch: any) => {
-    dispatch(loginSuccess({
-        payload: {
-            name,
-            age
-        }
-    }));
-};
+export const decreaseByAmount = createAction<number>("user/decreaseByAmountReducer");
 
-const logout = () => (dispatch: any) => {
-    dispatch(logoutSuccess());
-};
-
-export {
-    login,
-    logout
-};
+export const selectCount = (state: RootState) => state.user.count;
+export const selectIsLoading = (state: RootState) => state.user.isLoading;
