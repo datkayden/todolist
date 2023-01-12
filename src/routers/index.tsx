4f2c50ebc157642c130567fd8b50d9cfa4130bec
datkayden <thanhdatcntt301@gmail.com>
2023-01-12 12:53:08 +0700

    Add empty dashboard page

--- src/routers/index.tsx
+++ src/routers/index.tsx
@@ -1,16 +1,21 @@
 import React from "react";
 import List from "pages/List/List";
 import Login from "pages/Login/Login";
+import Dashboard from "pages/Dashboard/Dashboard";
 import {createBrowserRouter} from "react-router-dom";
 
 const router = createBrowserRouter([
     {
         path: "/",
-        element: <List/>
+        element: <Dashboard/>
     },
     {
         path: "/login",
         element: <Login/>
+    },
+    {
+        path: "/todo-list",
+        element: <List/>
     },
 ]);
 
