6b98fef9812b02dc3c45abb63f8f0c9d8ed5da16
datkayden <thanhdatcntt301@gmail.com>
2023-01-12 12:51:41 +0700

    Add login page

--- src/routers/index.tsx
+++ src/routers/index.tsx
@@ -1,12 +1,17 @@
 import React from "react";
 import List from "pages/List/List";
+import Login from "pages/Login/Login";
 import {createBrowserRouter} from "react-router-dom";
 
 const router = createBrowserRouter([
     {
         path: "/",
         element: <List/>
-    }
+    },
+    {
+        path: "/login",
+        element: <Login/>
+    },
 ]);
 
 export default router;
