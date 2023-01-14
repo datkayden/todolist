dba760101458feb7c89b180fc871e68062bf3a75
datkayden <thanhdatcntt301@gmail.com>
2023-01-14 12:58:57 +0700

    Add project route

--- src/routers/index.tsx
+++ src/routers/index.tsx
@@ -3,6 +3,7 @@ import List from "pages/List/List";
 import Login from "pages/Login/Login";
 import Dashboard from "pages/Dashboard/Dashboard";
 import Register from "pages/Register/Register";
+import Project from "pages/Project/Project";
 import {createBrowserRouter} from "react-router-dom";
 
 const router = createBrowserRouter([
@@ -21,6 +22,10 @@ const router = createBrowserRouter([
     {
         path: "/register",
         element: <Register/>
+    },
+    {
+        path: "/project",
+        element: <Project/>
     },
 ]);
 
