e27cfd12e5c0ec8ed1b090de022a270f08373216
datkayden <thanhdatcntt301@gmail.com>
2023-01-15 20:07:28 +0700

    Display and add new task in the UI

--- src/pages/List/List.tsx
+++ src/pages/List/List.tsx
@@ -1,16 +1,43 @@
-import React from "react";
+import React, {useState} from "react";
 import Container from "react-bootstrap/Container";
 import ListGroup from "react-bootstrap/ListGroup";
-import {useAppDispatch} from "store";
+import Form from "react-bootstrap/Form";
+import Button from "react-bootstrap/Button";
+import {useAppDispatch, useAppSelector} from "store";
+import {addNewTaskItem, selectTaskList} from "store/taskReducer";
+import {TaskItem} from "models/taskModel";
 
 const List = () => {
     const dispatch = useAppDispatch();
+    const taskList = useAppSelector(selectTaskList);
+    const [taskName, setTaskName] = useState<string>("");
+
+    const handleOnChange = (e: React.ChangeEvent<HTMLInputElement>): void => {
+        setTaskName(e.currentTarget.value);
+    };
+
+    const handleAddNewItem = (): void => {
+        const newItem: TaskItem = {
+            name: taskName,
+            isDone: false
+        };
+        dispatch(addNewTaskItem(newItem));
+        setTaskName("");
+    };
+
+    const isTaskNameValid: boolean = taskName.length > 0;
+
     return (
         <Container fluid="sm">
             <ListGroup>
-                <ListGroup.Item>Todo 1</ListGroup.Item>
-                <ListGroup.Item>Todo 2</ListGroup.Item>
+                {taskList.map(taskItem => (
+                    <ListGroup.Item key={taskItem.name}>{taskItem.name}</ListGroup.Item>
+                ))}
             </ListGroup>
+            <div>
+                <Form.Control value={taskName} onChange={handleOnChange} type="text" placeholder="Enter task name"/>
+                <Button disabled={!isTaskNameValid} onClick={handleAddNewItem} type="submit">Add new Item</Button>
+            </div>
         </Container>
     );
 };
