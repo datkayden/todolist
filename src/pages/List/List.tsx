bb2f756c0b4c656382d1da0c107349ec1bbf251e
datkayden <thanhdatcntt301@gmail.com>
2023-01-15 19:29:57 +0700

    Add test list group component in bootstrap

--- src/pages/List/List.tsx
+++ src/pages/List/List.tsx
@@ -1,60 +1,17 @@
 import React from "react";
-import {useAppDispatch, useAppSelector} from "store";
-import {
-    decrease,
-    decreaseByAmount,
-    getAsyncCount,
-    increase,
-    increaseByAmount,
-    selectCount,
-    selectIsLoading
-} from "store/userReducer";
+import Container from "react-bootstrap/Container";
+import ListGroup from "react-bootstrap/ListGroup";
+import {useAppDispatch} from "store";
 
 const List = () => {
     const dispatch = useAppDispatch();
-    const count = useAppSelector(selectCount);
-    const isLoading = useAppSelector(selectIsLoading);
-    const handleIncrease = () => {
-        dispatch(increase());
-    };
-
-    const handleDecrease = () => {
-        dispatch(decrease());
-    };
-
-    const handleIncreaseAmount = () => {
-        dispatch(increaseByAmount(2));
-    };
-
-    const handleDecreaseAmount = () => {
-        dispatch(decreaseByAmount(3));
-    };
-
-    const handleFetchUserId = () => {
-        dispatch(getAsyncCount(3));
-    };
-
     return (
-        <div>
-            <h2>Counter App</h2>
-            <div>Count: {count}</div>
-            <div>isLoading: {isLoading.toString()}</div>
-            <button onClick={handleIncrease}>
-                Increase
-            </button>
-            <button onClick={handleDecrease}>
-                Decrease
-            </button>
-            <button onClick={handleIncreaseAmount}>
-                Increase 2
-            </button>
-            <button onClick={handleDecreaseAmount}>
-                Decrease 3
-            </button>
-            <button onClick={handleFetchUserId}>
-                Increase Async 10
-            </button>
-        </div>
+        <Container fluid="sm">
+            <ListGroup>
+                <ListGroup.Item>Todo 1</ListGroup.Item>
+                <ListGroup.Item>Todo 2</ListGroup.Item>
+            </ListGroup>
+        </Container>
     );
 };
 
